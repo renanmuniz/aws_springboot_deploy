62c728a797f653ef764bff067726dd6ae53ab158
renan muniz merlo <renan.mmerlo@gmail.com>
2023-04-18 15:31:20 -0300

    criacao do projeto

---
+++ src/main/java/com/renan/springbootaws/SpringbootawsApplication.java
@@ -0,0 +1,13 @@
+package com.renan.springbootaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootawsApplication.class, args);
+	}
+
+}
